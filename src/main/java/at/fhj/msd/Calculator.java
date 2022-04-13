f219dcdc9acae10f061cdd4f0fd72c5bc3597d36
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2022-04-13 22:02:38 +0200

    added everything i forgot with add *

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,2 +1,42 @@
-package at.fhj.msd;public class Calculator {
+package at.fhj.msd;
+
+public class Calculator {
+
+    public Calculator() {
+
+    }
+
+    public double add(double number1, double number2) {
+        number1 += number2;
+        return number1;
+    }
+
+    public double minus(double number1, double number2) {
+        double num;
+        if(number1 > number2) {
+            num = number1 - number2;
+        }
+        else {
+            num = number2 - number1;
+        }
+        return num;
+    }
+
+    public double multiply(double number1, double number2) {
+        number1 *= number2;
+        return number1;
+    }
+
+    public double divide(double number1, double number2) {
+        double num = 0;
+        if((number1 != 0) && (number2 != 0)) {
+            if(number1 > number2) {
+                num = number1 / number2;
+            }
+            else {
+                num = number2 / number1;
+            }
+        }
+        return num;
+    }
 }
--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,2 +1,13 @@
-package at.fhj.msd;public class Main {
+package at.fhj.msd;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        double x = 10;
+        double y = 2;
+        calc.add(x, y);
+        calc.minus(x, y);
+        calc.multiply(x, y);
+        calc.divide(x, y);
+    }
 }

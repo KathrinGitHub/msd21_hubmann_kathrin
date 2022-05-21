5ab23e14430bd1fd4e3fa28c5783de10e98af2e8
Kathrin Hubmann <kathrin.hubmann@edu.fh-joanneum.at>
2022-05-21 16:31:09 +0200

    added two pictures, updated main class, gitignore and pom.xml

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,14 +1,23 @@
 package at.fhj.msd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
+        Logger logger = LogManager.getLogger();
+
         double x = 10;
         double y = 2;
         calc.add(x, y);
         calc.minus(x, y);
         calc.multiply(x, y);
+        logger.info("this is an info-logger");
+        logger.error("this is an error-logger");
         calc.divide(x, y);
         calc.faculty(y);
+
+
     }
 }
